ed067935084e253837c081d64ebcd6de29f97dbb
MKN3467 <mert.karan34@hotmail.com>
2025-10-28 16:51:48 +0100

    Lesson 4 2

--- content/day01/02/sketch.js
+++ content/day01/02/sketch.js
@@ -1,71 +1,86 @@
-let canvas, video;
+let particles = [];
+let numParticles = 50; // Adjust for density variation 1
+let maxHistory = 10; // How many previous positions to remember
 
-// Default P5 setup function
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  video = createCapture(VIDEO);
-  video.hide();
-}
-
-// Default P5 draw loop function
-function draw() {
-  drawImage(video);
-}
-
-function keyPressed() {
-  if (key == "s" || key == "S") saveImage(width, height);
-}
-
-// Tools
-
-// Make sketch full screen
-function goFullScreen() {
-  let isFullScreen = Boolean(fullscreen());
-  fullscreen(!isFullScreen);
-}
-
-// Resize canvas when the window is resized
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight, false);
-}
-
-// Sketch is double clicked
-function doubleClicked() {
-  goFullScreen();
-}
-
-// Timestamp
-function timestamp() {
-  return Date.now();
-}
-
-// Thumb
-function saveImage(w, h) {
-  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
-  save(img, `screenshot-${timestamp()}.jpg`);
-}
-
-// Draw centered full page image
-function drawImage(img) {
-  // var
-  let imgWidth = width;
-  let imgHeight = height;
-  let imgPosX = 0;
-  let imgPosY = 0;
-
-  // Calculate aspect ratios
-  const imgAspectRatio = img.width / img.height;
-  const sketchAspectRatio = width / height;
-
-  // Calculate img size and position
-  if (sketchAspectRatio >= imgAspectRatio) {
-    imgHeight = (img.height * width) / img.width;
-    imgPosY = -(imgHeight - height) / 2;
-  } else {
-    imgWidth = (img.width * height) / img.height;
-    imgPosX = -(imgWidth - width) / 2;
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector(0, 0);
+    this.r = 2;
+    this.history = []; // Drawing trail
   }
 
-  // Draw image
-  image(img, imgPosX, imgPosY, imgWidth, imgHeight);
+  // Calculate forces: mouse attraction and mutual repulsion
+  applyForces() {
+    // 1. Mouse Attraction
+    let mouse = createVector(mouseX, mouseY);
+    let dir = p5.Vector.sub(mouse, this.pos);
+    dir.setMag(0.1); // Adjust for attraction strength
+    this.acc.add(dir);
+
+    // 2. Mutual Repulsion (Constraint)
+    for (let other of particles) {
+      if (other !== this) {
+        let force = p5.Vector.sub(this.pos, other.pos);
+        let d = force.mag();
+        let min_d = 20; // Repulsion distance (adjust for variation 2)
+        if (d < min_d) {
+          force.setMag(-0.5 / (d * d)); // Repel with inverse-square-like force
+          this.acc.add(force);
+        }
+      }
+    }
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(5); // Speed limit (Constraint)
+    this.pos.add(this.vel);
+    this.acc.mult(0); // Reset acceleration
+
+    // Update history (Mark-Making)
+    this.history.push(this.pos.copy());
+    if (this.history.length > maxHistory) {
+      this.history.shift();
+    }
+  }
+
+  show() {
+    noFill();
+    beginShape();
+    
+    // Draw trail (Mark-Making: opacity based on age)
+    for (let i = 0; i < this.history.length; i++) {
+      let p = this.history[i];
+      let alpha = map(i, 0, maxHistory, 10, 255); // Older points are more transparent
+      let w = map(i, 0, maxHistory, 0.5, 2); // Thinner line for older points (Variation 3)
+      stroke(200, 150, 255, alpha); // Purple trail
+      strokeWeight(w);
+      vertex(p.x, p.y);
+    }
+    endShape();
+  }
 }
+
+function setup() {
+  createCanvas(800, 600);
+  for (let i = 0; i < numParticles; i++) {
+    particles.push(new Particle(random(width), random(height)));
+  }
+}
+
+function draw() {
+  background(0, 50); // Semi-transparent background for a fading effect
+
+  for (let p of particles) {
+    p.applyForces();
+    p.update();
+    p.show();
+  }
+}
+
+// *** To create 3 drawings: ***
+// 1. Change the number of particles (numParticles). (e.g., 10 for sparse, 100 for dense)
+// 2. Adjust the repulsion distance (min_d) inside Particle.applyForces(). (e.g., 5 for close, 50 for wide gaps)
+// 3. Change the line width mapping (w) and color inside Particle.show().
--- day04.md
+++ day04.md
@@ -63,3 +63,9 @@ What didn't work? Why do you think it failed? I initially tried to link color to
 {% raw %}
 <iframe src="./content/day01/01/embed.html" width="100%" height="450" frameborder="no"></iframe>
 {% endraw %} 
+
+![Example Image](./day04/lesson4_2.png) 
+
+{% raw %}
+<iframe src="./content/day01/02/embed.html" width="100%" height="450" frameborder="no"></iframe>
+{% endraw %} 
