a2920261c0c8dddd1610a39a005b950ddd37908a
MKN3467 <mert.karan34@hotmail.com>
2025-12-21 22:08:35 +0100

    images

--- content/day01/07/sketch.js
+++ content/day01/07/sketch.js
@@ -3,7 +3,7 @@ let img;
 function preload() {
   // Load the image you just uploaded
   // Make sure the path matches your folder structure!
-  img = loadImage('../007/portrait.jpg');
+  img = loadImage('portrait.jpg');
 }
 
 function setup() {
